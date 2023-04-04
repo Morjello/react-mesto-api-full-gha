b531d29d6a4b8aac69bf11ba3ce1b955f80e4171
Morjello <morjello@yandex.ru>
2023-04-04 22:37:24 +0300

    add logger

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const { requestLogger, errorLogger } = require("./middlewares/logger");
 
 const routes = require("./routes/index");
 const { PORT, DB_ADRESS } = require("./config");
@@ -13,9 +14,19 @@ mongoose.connect(DB_ADRESS, {
 });
 mongoose.set("strictQuery", false);
 
-app.use(cors({ origin: "http://localhost:3000" }));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(cors());
+app.use(requestLogger);
+
+app.get("/crash-test", () => {
+  setTimeout(() => {
+    throw new Error("Сервер сейчас упадёт");
+  }, 0);
+});
+
+app.use(errorLogger);
 app.use(routes);
 
 app.listen(PORT, () => {
